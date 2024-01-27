02639e32dd6fc191c8d46bc52548baae5ddedeb2
Anh Nguyen Van <anhnv.forwork@gmail.com>
2024-01-27 22:27:16 +0700

    feat: add getProfile enpoint api

--- server/src/main/java/com/nva/server/apis/AdminApi.java
+++ server/src/main/java/com/nva/server/apis/AdminApi.java
@@ -2,6 +2,8 @@ package com.nva.server.apis;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,5 +15,11 @@ public class AdminApi {
     @GetMapping("/sayHello")
     public ResponseEntity<String> sayHello() {
         return ResponseEntity.ok("Admin hello!");
+    }
+
+    @GetMapping("/profile")
+    public ResponseEntity<?> getProfile() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        return ResponseEntity.ok().body(authentication);
     }
 }
