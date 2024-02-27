38f7a97a66ef83b279536514c97952f0c283bc58
Anh Nguyen Van <anhnv.forwork@gmail.com>
2024-02-27 10:43:23 +0700

    feat: add toast

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -23,8 +23,10 @@ import {
 } from "../lib/redux/actions/Conversation";
 import SuggestedQuestionSection from "./SuggestedQuestionSection";
 import MessageCard from "./MessageCard";
-import { CONVERSATION_PAGE_SIZE } from '../lib/constants/index';
+import { CONVERSATION_PAGE_SIZE } from "../lib/constants/index";
 import "./style.scss";
+import CustomToast from "@/lib/components/toast";
+import { ToastInformation } from "./auth/module";
 
 const HomePage = () => {
   const dispatch = useDispatch<AppDispatch>();
@@ -41,6 +43,8 @@ const HomePage = () => {
   const [question, setQuestion] = useState("");
   const [pageNumber, setPageNumber] = useState(1);
   const [data, setData] = useState<Message[]>([]);
+  const [openToast, setOpenToast] = useState(false);
+  const [toastInfo, setToastInfo] = useState<ToastInformation>();
 
   const handleQuestionChange = (
     e: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
@@ -52,7 +56,10 @@ const HomePage = () => {
     if (messages?.hasNext) {
       setPageNumber((prev) => prev + 1);
       dispatch(
-        getConversationMessagesThunk({ pageNumber: pageNumber + 1, pageSize: CONVERSATION_PAGE_SIZE })
+        getConversationMessagesThunk({
+          pageNumber: pageNumber + 1,
+          pageSize: CONVERSATION_PAGE_SIZE,
+        })
       );
       if (scrollEndRef.current) {
         scrollEndRef.current.scrollIntoView({ behavior: "smooth" });
@@ -71,7 +78,12 @@ const HomePage = () => {
 
   // Fetch initial messages
   useEffect(() => {
-    dispatch(getConversationMessagesThunk({ pageNumber, pageSize: CONVERSATION_PAGE_SIZE }));
+    dispatch(
+      getConversationMessagesThunk({
+        pageNumber,
+        pageSize: CONVERSATION_PAGE_SIZE,
+      })
+    );
   }, []);
 
   useEffect(() => {
@@ -84,6 +96,13 @@ const HomePage = () => {
             )
         );
         return [...newMessages.reverse(), ...prev];
+      });
+    } else if (errorGetMessages) {
+      setOpenToast(true);
+      setToastInfo({
+        title: "Thất bại",
+        message: errorGetMessages,
+        severity: "error",
       });
     }
   }, [messages, errorGetMessages]);
@@ -98,148 +117,169 @@ const HomePage = () => {
     if (returnedResult) {
       data.pop();
       setData((prev) => [...prev, returnedResult]);
-    } else if (errorGetMessages) {
+    } else if (errorSendQuery) {
+      setOpenToast(true);
+      setToastInfo({
+        title: "Thất bại",
+        message: errorSendQuery,
+        severity: "error",
+      });
     }
   }, [returnedResult, errorSendQuery]);
 
   return (
-    <CustomLayout>
-      <Box className="home-page-container">
-        <Stack
-          direction="column"
-          justifyContent="space-between"
-          alignItems="center"
-          className="home-page-conversation"
-          gap={1}
-        >
-          <Stack direction="column" className="home-page-conversation-messages">
-            {loadingMessages ? (
-              <Stack
-                justifyContent="center"
-                alignItems="center"
-                sx={{ width: "100%", height: "100%" }}
-              >
-                <CircularProgress sx={{ color: "var(--primary)" }} />
-              </Stack>
-            ) : data.length === 0 ? (
-              <EmptyDataPlaceholder />
-            ) : (
-              <Stack
-                direction="column"
-                gap={2}
-                sx={{ width: "100%", height: "100%" }}
-              >
-                {data.map((message, index) => {
-                  if (index === 0)
-                    return (
-                      <Stack
-                        direction="column"
-                        width="100%"
-                        height="inherit"
-                        gap={3}
-                        key={index}
-                      >
-                        {messages?.hasNext && (
-                          <Button
-                            variant="outlined"
-                            color="primary"
-                            onClick={handleLoadMoreMessage}
-                            sx={{
-                              width: "fit-content",
-                              height: "32px",
-                              alignSelf: "center",
-                            }}
-                          >
-                            Xem thêm lịch sử
-                          </Button>
-                        )}
-                        <Stack direction="column" width="100%" gap={2}>
+    <>
+      <CustomLayout>
+        <Box className="home-page-container">
+          <Stack
+            direction="column"
+            justifyContent="space-between"
+            alignItems="center"
+            className="home-page-conversation"
+            gap={1}
+          >
+            <Stack
+              direction="column"
+              className="home-page-conversation-messages"
+            >
+              {loadingMessages ? (
+                <Stack
+                  justifyContent="center"
+                  alignItems="center"
+                  sx={{ width: "100%", height: "100%" }}
+                >
+                  <CircularProgress sx={{ color: "var(--primary)" }} />
+                </Stack>
+              ) : data.length === 0 ? (
+                <EmptyDataPlaceholder />
+              ) : (
+                <Stack
+                  direction="column"
+                  gap={2}
+                  sx={{ width: "100%", height: "100%" }}
+                >
+                  {data.map((message, index) => {
+                    if (index === 0)
+                      return (
+                        <Stack
+                          direction="column"
+                          width="100%"
+                          height="inherit"
+                          gap={3}
+                          key={index}
+                        >
+                          {messages?.hasNext && (
+                            <Button
+                              variant="outlined"
+                              color="primary"
+                              onClick={handleLoadMoreMessage}
+                              sx={{
+                                width: "fit-content",
+                                height: "32px",
+                                alignSelf: "center",
+                              }}
+                            >
+                              Xem thêm lịch sử
+                            </Button>
+                          )}
+                          <Stack direction="column" width="100%" gap={2}>
+                            <MessageCard
+                              type="question"
+                              content={message.question}
+                            />
+                            <MessageCard
+                              type="answer"
+                              content={message.answer}
+                            />
+                          </Stack>
+                        </Stack>
+                      );
+                    else if (index === data.length - 1)
+                      return (
+                        <Stack
+                          direction="column"
+                          width="100%"
+                          gap={2}
+                          key={index}
+                        >
+                          <MessageCard
+                            type="question"
+                            content={message.question}
+                          />
+                          <MessageCard type="answer" content={message.answer} />
+                          <div ref={scrollEndRef} />
+                        </Stack>
+                      );
+                    else
+                      return (
+                        <Stack
+                          direction="column"
+                          width="100%"
+                          gap={2}
+                          key={index}
+                        >
                           <MessageCard
                             type="question"
                             content={message.question}
                           />
                           <MessageCard type="answer" content={message.answer} />
                         </Stack>
-                      </Stack>
-                    );
-                  else if (index === data.length - 1)
-                    return (
-                      <Stack
-                        direction="column"
-                        width="100%"
-                        gap={2}
-                        key={index}
-                      >
-                        <MessageCard
-                          type="question"
-                          content={message.question}
-                        />
-                        <MessageCard type="answer" content={message.answer} />
-                        <div ref={scrollEndRef} />
-                      </Stack>
-                    );
-                  else
-                    return (
-                      <Stack
-                        direction="column"
-                        width="100%"
-                        gap={2}
-                        key={index}
-                      >
-                        <MessageCard
-                          type="question"
-                          content={message.question}
-                        />
-                        <MessageCard type="answer" content={message.answer} />
-                      </Stack>
-                    );
-                })}
-              </Stack>
-            )}
-          </Stack>
+                      );
+                  })}
+                </Stack>
+              )}
+            </Stack>
 
-          <Box className="home-page-conversation-actions">
-            <Box className="chat-input-field">
-              <Typography
-                className="chat-input-field-placeholder"
-                display={question === "" ? "flex" : "none"}
-              >
-                Nhập câu hỏi của bạn tại đây...
-              </Typography>
-              <TextField
-                type="text"
-                multiline
-                maxRows={4}
-                variant="outlined"
-                onChange={handleQuestionChange}
-                value={question}
-                InputProps={{
-                  endAdornment: (
-                    <Stack
-                      direction="row"
-                      gap={1}
-                      height="100%"
-                      alignItems="flex-end"
-                      marginLeft={1}
-                    >
-                      <Button
-                        variant="contained"
-                        color="primary"
-                        disabled={question === ""}
-                        onClick={handleSendQuery}
+            <Box className="home-page-conversation-actions">
+              <Box className="chat-input-field">
+                <Typography
+                  className="chat-input-field-placeholder"
+                  display={question === "" ? "flex" : "none"}
+                >
+                  Nhập câu hỏi của bạn tại đây...
+                </Typography>
+                <TextField
+                  type="text"
+                  multiline
+                  maxRows={4}
+                  variant="outlined"
+                  onChange={handleQuestionChange}
+                  value={question}
+                  InputProps={{
+                    endAdornment: (
+                      <Stack
+                        direction="row"
+                        gap={1}
+                        height="100%"
+                        alignItems="flex-end"
+                        marginLeft={1}
                       >
-                        <PaperPlaneRight size={20} />
-                      </Button>
-                      <SuggestedQuestionSection setQuestion={setQuestion} />
-                    </Stack>
-                  ),
-                }}
-              />
+                        <Button
+                          variant="contained"
+                          color="primary"
+                          disabled={question === ""}
+                          onClick={handleSendQuery}
+                        >
+                          <PaperPlaneRight size={20} />
+                        </Button>
+                        <SuggestedQuestionSection setQuestion={setQuestion} />
+                      </Stack>
+                    ),
+                  }}
+                />
+              </Box>
             </Box>
-          </Box>
-        </Stack>
-      </Box>
-    </CustomLayout>
+          </Stack>
+        </Box>
+      </CustomLayout>
+      <CustomToast
+        open={openToast}
+        title={toastInfo?.title || ''}
+        handleClose={() => setOpenToast(false)}
+        message={toastInfo?.message || ''}
+        severity={toastInfo?.severity}
+      />
+    </>
   );
 };
 
